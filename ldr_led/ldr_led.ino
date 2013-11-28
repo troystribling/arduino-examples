b2520f6b3abb56b0bed06b3b59583c659e6590b5
Troy Stribling <troy.stribling@gmail.com>
2013-11-27 22:27:39 -0800

    first commit

---
+++ ldr_led/ldr_led.ino
@@ -0,0 +1,24 @@
+// light_led.ino
+
+const int ledPin        = 13;   	// LED connected to digital pin 13
+const int sensorPin     = 0;     	// connect sensor to analog input 0
+
+const int minDuration   = 100;   	// minimum wait between blinks
+const int maxDuration   = 1000;  	// maximum wait between blinks
+
+void setup() {
+	pinMode(ledPin, OUTPUT); 	// enable output on the led pin
+	Serial.begin(9600); 		// initialize Serial
+}
+
+void loop() {
+	int rate = analogRead(sensorPin); 						// read the analog input
+	rate = map(rate, 200, 800, minDuration, maxDuration);	// convert to blink rate
+	rate = constrain(rate, minDuration, maxDuration); 		// constrain the value
+	Serial.println(rate);									// print rate to serial port
+	digitalWrite(ledPin, HIGH); 							// set the LED off delay( rate);
+	delay(rate); 											// wait duration dependent on light level
+	digitalWrite(ledPin, LOW); 								// set the LED off delay( rate);
+	delay(rate); 											// wait duration dependent on light level
+}
+
