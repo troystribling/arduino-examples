9b33a11cde0055abfa15c8bd531937e8f5ce19c9
Troy Stribling <troy.stribling@gmail.com>
2013-12-01 18:17:13 -0800

    added analog input and voltage moitor examples

---
+++ analog_input/analog_input.ino
@@ -0,0 +1,23 @@
+// analog_input.ino
+//  use a potentiometer to control the lash duration of an led
+
+const int ledPin    = 13;
+const int potPin    = 0;
+const int maxDelay  = 1000;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int val = analogRead(potPin);
+  int percent = map(val, 0, 1023, 0, 100);
+  int delayVal = maxDelay*percent/100;
+  digitalWrite(ledPin, HIGH);
+  delay(delayVal);
+  digitalWrite(ledPin, LOW);
+  delay(maxDelay - delayVal);
+  Serial.println(percent);
+}
+
---
+++ monitor_voltage/monitor_voltage.ino
@@ -0,0 +1,17 @@
+// monitor_voltage.ino
+// report a measured voltage
+
+const int batterPin           = 0;
+const float referenceVoltage  = 5.0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int val = analogRead(batterPin);
+  float volts = referenceVoltage*(val/1023.0);
+  Serial.println(volts);
+  delay(2000);
+}
+
