b995bbf6acf32192524f24b4101cae304d4b5cef
Troy Stribling <troy.stribling@gmail.com>
2013-12-08 17:48:02 -0800

    added display column flasher

--- dot_matrix_led_display_flash_columns/dot_matrix_led_display_flash_columns.ino
+++ dot_matrix_led_display_flash_columns/dot_matrix_led_display_flash_columns.ino
@@ -1,5 +1,5 @@
-// dot_matrix_led_display_flash_rows.ino
-// Flash the red/orange rows on the Lite On two color 5x7 LED dot matrix
+// dot_matrix_led_display_flash_columns.ino
+// Flash the red/orange columns on the Lite On two color 5x7 LED dot matrix
 // http://www.jameco.com/webapp/wcs/stores/servlet/ProductDisplay?langId=-1&storeId=10001&catalogId=10001&productId=1955159
 
 const int columnPins[] = {2, 3, 4, 5, 6};
@@ -8,7 +8,7 @@ const int rowPins[] = {9, 10, 11, 12, 15, 16, 17};
 const int NUMBER_OF_COLUMNS = sizeof(columnPins)/sizeof(columnPins[0]);
 const int NUMBER_OF_ROWS = sizeof(rowPins)/sizeof(rowPins[0]);
 
-int currentRow = 1;
+int currentColumn = 1;
 
 void setup() {
   Serial.begin(9600);
@@ -18,33 +18,32 @@ void setup() {
   for (int j = 0; j < NUMBER_OF_COLUMNS; j++) {
     pinMode(columnPins[j], OUTPUT);
   }
-  setDisplayState(LOW);
-  setRowState(currentRow - 1, HIGH);
+  setDisplayState(HIGH);
+  setColumnState(currentColumn - 1, LOW);
   delay(5000);
 }
 
 void setDisplayState(int state) {
   for (int i = 0; i < NUMBER_OF_ROWS; i++) {
-    digitalWrite(rowPins[i], state);
+    digitalWrite(rowPins[i], HIGH);
   }
   for (int i = 0; i < NUMBER_OF_COLUMNS; i++) {
-    digitalWrite(columnPins[i], LOW);
+    digitalWrite(columnPins[i], state);
   }
 }
 
-void setRowState(int row, int state) {
-  digitalWrite(rowPins[row], state);
+void setColumnState(int column, int state) {
+  digitalWrite(columnPins[column], state);
 }
 
 void loop() {
-  Serial.print("Turn off row: "); Serial.println(currentRow);
-  setRowState(currentRow - 1, LOW);
-  if (currentRow == NUMBER_OF_ROWS) {
-    currentRow = 0;
+  Serial.print("Turn off column: "); Serial.println(currentColumn);
+  setColumnState(currentColumn - 1, HIGH);
+  if (currentColumn == NUMBER_OF_COLUMNS) {
+    currentColumn = 0;
   }
-  Serial.print("Turn on row: "); Serial.println(currentRow+1);
-  setRowState(currentRow, HIGH);
-  currentRow++;
+  Serial.print("Turn on column: "); Serial.println(currentColumn+1);
+  setColumnState(currentColumn, LOW);
+  currentColumn++;
   delay(5000);
 }
-
