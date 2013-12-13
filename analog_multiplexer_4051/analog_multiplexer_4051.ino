2f1d4d759b635987243b8bd177900f4f3c575369
Troy Stribling <troy.stribling@gmail.com>
2013-12-12 22:38:00 -0800

    more work on anolog multiplexer example

--- analog_multiplexer_4051/analog_multiplexer_4051.ino
+++ analog_multiplexer_4051/analog_multiplexer_4051.ino
@@ -9,7 +9,7 @@ const int bluePin       = 6;
 const int inputPin      = 0;
 
 const int channelPins[] = {7, 8, 9};
-const int channelCount  = 3;
+const int channelCount  = sizeof(channelPins)/sizeof(channelPins[0]);;
 
 const int redChannel    = 0;
 const int greenChannel  = 1;
@@ -29,21 +29,32 @@ void updateLed(int red, int green, int blue) {
 }
 
 int readChannel(int channelNumber) {
+  Serial.print("CHANNEL: ");Serial.println(channelNumber);
   for (int bit = 0; bit < 3; bit++) {
+    Serial.print("BIT: ");Serial.println(bit);
     int pin = channelPins[bit];
+    Serial.print("PIN: ");Serial.println(pin);
     int isBitSet = bitRead(channelNumber, bit);
+    Serial.print("BIT VALUE: ");Serial.println(isBitSet);
     digitalWrite(pin, isBitSet);
   }
   int val = analogRead(inputPin);
+  Serial.print("RAW VALUE: ");Serial.println(val);
   val = map(val, 0, 1023, 0, 255);
+  Serial.print("SCALED VALUE: ");Serial.println(val);
   return constrain(val, 0, 255);
 
 }
 
 void loop() {
+  Serial.println("");
   int red = readChannel(redChannel);
   int green = readChannel(greenChannel);
   int blue = readChannel(blueChannel);
+  Serial.print("RED: ");Serial.println(red);
+  Serial.print("GREEN: ");Serial.println(green);
+  Serial.print("BLUE: ");Serial.println(blue);
   updateLed(red, green, blue);
+  delay(1000);
 }
 
