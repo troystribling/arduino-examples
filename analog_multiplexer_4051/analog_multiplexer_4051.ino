2c18a6f1fa44c12535b69ef395c7eff0b94a0c3c
Troy Stribling <troy.stribling@gmail.com>
2013-12-13 07:34:47 -0800

    4051 multiplexer example works

--- analog_multiplexer_4051/analog_multiplexer_4051.ino
+++ analog_multiplexer_4051/analog_multiplexer_4051.ino
@@ -9,10 +9,10 @@ const int bluePin       = 6;
 const int inputPin      = 0;
 
 const int channelPins[] = {7, 8, 9};
-const int channelCount  = sizeof(channelPins)/sizeof(channelPins[0]);;
+const int channelCount  = sizeof(channelPins)/sizeof(channelPins[0]);
 
 const int redChannel    = 0;
-const int greenChannel  = 1;
+const int greenChannel  = 5;
 const int blueChannel   = 2;
 
 void setup() {
@@ -29,19 +29,13 @@ void updateLed(int red, int green, int blue) {
 }
 
 int readChannel(int channelNumber) {
-  Serial.print("CHANNEL: ");Serial.println(channelNumber);
   for (int bit = 0; bit < 3; bit++) {
-    Serial.print("BIT: ");Serial.println(bit);
     int pin = channelPins[bit];
-    Serial.print("PIN: ");Serial.println(pin);
     int isBitSet = bitRead(channelNumber, bit);
-    Serial.print("BIT VALUE: ");Serial.println(isBitSet);
     digitalWrite(pin, isBitSet);
   }
   int val = analogRead(inputPin);
-  Serial.print("RAW VALUE: ");Serial.println(val);
   val = map(val, 0, 1023, 0, 255);
-  Serial.print("SCALED VALUE: ");Serial.println(val);
   return constrain(val, 0, 255);
 
 }
