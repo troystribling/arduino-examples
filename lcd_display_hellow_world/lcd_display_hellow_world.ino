6e728b9392b9c030f781e5eac732fa29918acf60
Troy Stribling <troy.stribling@gmail.com>
2013-12-14 13:37:31 -0800

    write something to rows 3 and 4

--- lcd_display_hellow_world/lcd_display_hellow_world.ino
+++ lcd_display_hellow_world/lcd_display_hellow_world.ino
@@ -10,12 +10,19 @@ const int numCols   = 16;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
+  lcd.setCursor(0, 0);
   lcd.begin(numCols, numRows);
   lcd.print("hello, world!");
+  lcd.setCursor(-4, 2);
+  lcd.print("row 3");
+  lcd.setCursor(-4, 3);
+  lcd.print("row 4");
 }
 
 void loop() {
   lcd.setCursor(0, 1);
+  lcd.print("uptime: ");
+  lcd.setCursor(7, 1);
   lcd.print(millis()/1000);
 }
 
