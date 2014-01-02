c979428c0f247341d2660c520e4c79151ebfa54d
Troy Stribling <troy.stribling@gmail.com>
2014-01-02 08:14:32 -0800

    interupt examples work

--- attach_interupt/attach_interupt.ino
+++ attach_interupt/attach_interupt.ino
@@ -4,17 +4,18 @@
 int pin = 13;
 volatile int state = LOW;
 
-void setup()
-{
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Start Test");
   pinMode(pin, OUTPUT);
   attachInterrupt(0, blink, CHANGE);
 }
 
-void loop()
-{
+void loop() {
   digitalWrite(pin, state);
 }
 
 void blink() {
   state = !state;
+  Serial.print("blink: ");Serial.println(state);
 }
