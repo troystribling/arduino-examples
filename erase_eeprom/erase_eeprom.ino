be67fcf3f79249643e2ebac427effdf1aeb924d9
Troy Stribling <troy.stribling@gmail.com>
2014-01-26 12:49:50 -0800

    added eeprom eraser

---
+++ erase_eeprom/erase_eeprom.ino
@@ -0,0 +1,23 @@
+// erase_eeprom.ino
+#include <EEPROM.h>
+
+#define NUMBER_BYTES 1024
+
+int addr = 0;
+
+void eraseEEPROM() {
+  Serial.println("Start erase");
+  for (int i = 0; i < NUMBER_BYTES; i++) {
+    EEPROM.write(addr + i, 0);
+  }
+  Serial.println("End erase");
+}
+
+void setup() {
+  Serial.begin(9600);
+  eraseEEPROM();
+}
+
+void loop() {
+  delay(1000);
+}
