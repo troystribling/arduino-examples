9f85483537f20b90b65c9a34bc674a9e1c60d00d
Troy Stribling <troy.stribling@gmail.com>
2014-04-06 14:40:46 -0700

    added I2C request/response example

---
+++ i2c_master/i2c_master.ino
@@ -0,0 +1,38 @@
+/* * I2C_Master * Echo Serial data to an I2C slave */
+#include <Wire.h>
+
+const int address = 4;
+const char* message = "Hello-";
+
+// the address to be used by the communicating devices
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(1000);
+  writeMessage();
+  readResponse(1);
+}
+
+void writeMessage() {
+  Serial.print("writeMessage: ");Serial.println(message);
+  Wire.beginTransmission(address);
+  for (int i = 0; i < strlen(message); i++) {
+    Wire.write(message[i]);
+  }
+  Wire.endTransmission();
+  Serial.println("message sent");
+}
+
+void readResponse(int8_t message_size) {
+  Serial.println("readResponse");
+  Wire.requestFrom(address, message_size);
+  while(Wire.available()) {
+    char c = Wire.read();
+    Serial.print("received: ");Serial.println(c, HEX);
+  }
+  Serial.println("finished");
+}
---
+++ i2c_slave/i2c_slave.ino
@@ -0,0 +1,32 @@
+/* * I2C_Slave * monitors I2C requests and echoes these to the serial port */
+
+#include <Wire.h>
+const int address = 4;
+
+// the address to be used by the communicating devices
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Slave Starting");
+  Wire.begin(address);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+}
+
+void loop() {
+}
+
+void receiveEvent(int howMany) {
+  Serial.println("receiveEvent");
+  while(Wire.available() > 0) {
+    char c = Wire.read();
+    Serial.write(c);
+  }
+  Serial.println("finished");
+}
+
+void requestEvent() {
+  Serial.println("Master Request Received");
+  Wire.write(0x01);
+  Serial.println("finished");
+}
